2675d9c4be9b859c39c5069726e267de82f05de4
Jorge Giovannetti <giovannetti2000@gmail.com>
2023-01-02 05:21:38 -0600

    Layout responsiveness cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
-  <main class="p-5 bg-zinc-50 dark:bg-zinc-800 min-h-screen text-center">
+  <main class="p-5 flex flex-col bg-zinc-50 dark:bg-zinc-800 min-h-screen text-center items-center">
     <h1 class="mb-8 text-3xl font-bold dark:text-white">Memory Game</h1>
     <h3 class="text-xl font-bold mb-4 dark:text-white">Turns:</h3>
-    <div class="grid grid-cols-5 xs:grid-cols-2 gap-4">
+    <div class="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-5 gap-4">
       <div v-for="card in cards">
         <Card :data="card" class="" />
       </div>
