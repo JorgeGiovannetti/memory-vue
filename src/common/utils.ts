531b33e3b51679bb7c37c13846375ee938765fd1
Jorge Giovannetti <giovannetti2000@gmail.com>
2023-01-02 16:11:27 -0500

    Shuffle return type depends on parameter

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -1,10 +1,8 @@
-
 // Implementation of Fisher-Yates shuffle algorithm
-export function shuffle(array: any[]) {
-    for (let i = array.length - 1; i > 0; i--) {
-        let j: number = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
+export function shuffle<T>(array: T[]): T[] {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j: number = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
 }
-
